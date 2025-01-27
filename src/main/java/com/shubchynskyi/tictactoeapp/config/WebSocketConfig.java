8ec53e6a677f16e8fc14c30e781a83fade9804ba
Shubchynskyi <shubchinskiydmitriy@gmail.com>
2025-01-27 16:16:40 +0100

    try with pwa (add domain name to websocket)

--- src/main/java/com/shubchynskyi/tictactoeapp/config/WebSocketConfig.java
+++ src/main/java/com/shubchynskyi/tictactoeapp/config/WebSocketConfig.java
@@ -28,7 +28,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint(wsEndpoint)
-//                .setAllowedOrigins("https://tic-tac-toe.shubchynskyi.pp.ua") // todo rm
+                .setAllowedOrigins("https://tic-tac-toe.shubchynskyi.pp.ua") // todo to config
                 .withSockJS();
     }
 
